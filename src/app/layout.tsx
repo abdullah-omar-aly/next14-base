13780cde91b8016c14edb74881877b1f2435e445
Abdullah Omar <abdullahomar.dev@gmail.com>
2023-12-09 19:33:14 +0200

    configure dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from 'next'
-// import { Inter } from 'next/font/google'
 import { Inter as FontSans } from "next/font/google"
 
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/theme-provider'
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
-
-// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -28,7 +26,16 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
-      >{children}</body>
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
